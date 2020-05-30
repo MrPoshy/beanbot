3653b3e75dbae6cacfdd413fcd23f5978b0b38ac
juanrtech <14365617+JuanRTech@users.noreply.github.com>
2020-05-30 04:48:15 +0000

    Added guild connection info

--- bot/bot.py
+++ bot/bot.py
@@ -5,13 +5,24 @@ import os
 import discord
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
+# Create connection
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} is in!')
+    # Connect to all guilds
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is in!\n'
+        f'Connected to: {guild.name}(id: {guild.id})'
+    )
 
 client.run(TOKEN)
