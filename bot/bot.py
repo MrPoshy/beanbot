b99824f3e135e84293980772cd008b578e9c024b
juanrtech <14365617+JuanRTech@users.noreply.github.com>
2020-05-30 04:35:16 +0000

    Created bot.py with basic connection test

---
+++ bot/bot.py
@@ -0,0 +1,17 @@
+# bot.py
+# BeanBot Main
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is in!')
+
+client.run(TOKEN)
